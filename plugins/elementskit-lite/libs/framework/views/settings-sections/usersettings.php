06d8f2526f95dff400905148552d4522c8ebc71a
Sourav Ghosh <souravghosh241170@gmail.com>
2024-08-15 11:35:58 +0530

    Update usersettings.php

--- plugins/elementskit-lite/libs/framework/views/settings-sections/usersettings.php
+++ plugins/elementskit-lite/libs/framework/views/settings-sections/usersettings.php
@@ -645,7 +645,6 @@ $pro_active = ( in_array( 'elementskit/elementskit.php', apply_filters( 'active_
 								'type'        => 'text',
 								'name'        => 'user_data[google_map][api_key]',
 								'label'       => esc_html__( 'Api Key', 'elementskit-lite' ),
-								'placeholder' => 'AIzaSyA-10-OHpfss9XvUDWILmos62MnG_L4MYw',
 								'value'       => ( ! isset( $user_data['google_map']['api_key'] ) ) ? '' : ( $user_data['google_map']['api_key'] ),
 							)
 						);
